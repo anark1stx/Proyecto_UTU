3e987b5dffa1946e31f3bd4f7da21c99bd1a01ea
Mateo <mateosabe66@gmail.com>
2020-09-02 13:57:50 -0300

    Actualización de clase enfermedad

--- Entidades/MedicoUtils/E_Enfermedad.vb
+++ Entidades/MedicoUtils/E_Enfermedad.vb
@@ -1,7 +1,6 @@
 ﻿Public Class E_Enfermedad
     Protected _nombre As String
     Protected _descripcion As String
-    Protected _categorias As List(Of Categoria)
     Property Nombre As String
         Get
             Return _nombre
@@ -20,45 +19,35 @@
         End Set
     End Property
 
-    Property Categorias As List(Of Categoria)
-        Get
-            Return _categorias
-        End Get
-        Set(value As List(Of Categoria))
-            _categorias = value
-        End Set
-    End Property
-
     Public Class Clasificacion
-        Protected _nombre
-        Protected _descripcion
-        Protected _categoria As Categoria
-        Property Nombre As String
+        Protected _nombre_e As String
+        Protected _nombre_cat As String
+        Property NombreEnfermedad As String
             Get
-                Return _nombre
+                Return _nombre_e
             End Get
             Set(value As String)
-                _nombre = value
+                _nombre_e = value
             End Set
         End Property
 
-        Property Descripcion As String
+        Property NombreCategoria As String
             Get
-                Return _nombre
+                Return _nombre_cat
             End Get
             Set(value As String)
-                _nombre = value
+                _nombre_cat = value
             End Set
         End Property
 
-        Property Categoria As Categoria
-            Get
-                Return _categoria
-            End Get
-            Set(value As Categoria)
-                _categoria = value
-            End Set
-        End Property
+        Sub New()
+
+        End Sub
+
+        Sub New(nombre_e As String, nom_categoria As String)
+            _nombre_e = nombre_e
+            _nombre_cat = nom_categoria
+        End Sub
 
     End Class
     Public Class Categoria
@@ -82,8 +71,58 @@
             End Set
         End Property
 
+        Sub New()
+
+        End Sub
+
+        Sub New(nombre As String, descripcion As String)
+            _nombre = nombre
+            _descripcion = descripcion
+        End Sub
+
+        Public Class SubCategoria
+            Protected _categoria_padre As String
+            Protected _nombre As String
+            Protected _descripcion As String
+
+            Property CategoriaPadre As String
+                Get
+                    Return _categoria_padre
+                End Get
+                Set(value As String)
+                    _categoria_padre = value
+                End Set
+            End Property
+
+            Property Nombre As String
+                Get
+                    Return _nombre
+                End Get
+                Set(value As String)
+                    _nombre = value
+                End Set
+            End Property
+            Property Descripcion As String
+                Get
+                    Return _descripcion
+                End Get
+                Set(value As String)
+                    _descripcion = value
+                End Set
+            End Property
+
+            Sub New()
+
+            End Sub
+
+            Sub New(categoria_padre As String, nombre As String, descripcion As String)
+                _categoria_padre = categoria_padre
+                _nombre = nombre
+                _descripcion = descripcion
+            End Sub
+
+        End Class
+
     End Class
-
-
 
 End Class
