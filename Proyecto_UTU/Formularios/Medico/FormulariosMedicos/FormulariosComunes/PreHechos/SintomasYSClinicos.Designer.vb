37aa78e1beff18de138fb62b83d96d71a6a57693
Mateo <mateosabe66@gmail.com>
2020-09-13 18:42:10 -0300

    diseño responsive para el tab de sc y sintomas

--- Proyecto_UTU/Formularios/Medico/FormulariosMedicos/FormulariosComunes/PreHechos/SintomasYSClinicos.Designer.vb
+++ Proyecto_UTU/Formularios/Medico/FormulariosMedicos/FormulariosComunes/PreHechos/SintomasYSClinicos.Designer.vb
@@ -26,18 +26,24 @@ Partial Class SintomasYSClinicos
         Me.TabControlFormulario = New System.Windows.Forms.TabControl()
         Me.tbpEntrevista = New System.Windows.Forms.TabPage()
         Me.tbpSintomasYsg = New System.Windows.Forms.TabPage()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.btnBorrarSintoma = New System.Windows.Forms.Button()
-        Me.btnBorrarSigno = New System.Windows.Forms.Button()
         Me.btnAgregarSintoma = New System.Windows.Forms.Button()
         Me.lblIngreseSintoma = New System.Windows.Forms.Label()
         Me.txtSintoma = New System.Windows.Forms.TextBox()
         Me.lbSintomas = New System.Windows.Forms.ListBox()
+        Me.pnlSigno = New System.Windows.Forms.Panel()
+        Me.btnBorrarSigno = New System.Windows.Forms.Button()
         Me.btnAgregarSClinico = New System.Windows.Forms.Button()
         Me.lblIngreseSignoClinico = New System.Windows.Forms.Label()
         Me.txtSgClinico = New System.Windows.Forms.TextBox()
         Me.lbSignosClinicos = New System.Windows.Forms.ListBox()
         Me.TabControlFormulario.SuspendLayout()
         Me.tbpSintomasYsg.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.pnlSigno.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControlFormulario
@@ -65,16 +71,7 @@ Partial Class SintomasYSClinicos
         'tbpSintomasYsg
         '
         Me.tbpSintomasYsg.BackColor = System.Drawing.Color.LightBlue
-        Me.tbpSintomasYsg.Controls.Add(Me.btnBorrarSintoma)
-        Me.tbpSintomasYsg.Controls.Add(Me.btnBorrarSigno)
-        Me.tbpSintomasYsg.Controls.Add(Me.btnAgregarSintoma)
-        Me.tbpSintomasYsg.Controls.Add(Me.lblIngreseSintoma)
-        Me.tbpSintomasYsg.Controls.Add(Me.txtSintoma)
-        Me.tbpSintomasYsg.Controls.Add(Me.lbSintomas)
-        Me.tbpSintomasYsg.Controls.Add(Me.btnAgregarSClinico)
-        Me.tbpSintomasYsg.Controls.Add(Me.lblIngreseSignoClinico)
-        Me.tbpSintomasYsg.Controls.Add(Me.txtSgClinico)
-        Me.tbpSintomasYsg.Controls.Add(Me.lbSignosClinicos)
+        Me.tbpSintomasYsg.Controls.Add(Me.TableLayoutPanel1)
         Me.tbpSintomasYsg.Location = New System.Drawing.Point(4, 25)
         Me.tbpSintomasYsg.Name = "tbpSintomasYsg"
         Me.tbpSintomasYsg.Padding = New System.Windows.Forms.Padding(3)
@@ -82,103 +79,160 @@ Partial Class SintomasYSClinicos
         Me.tbpSintomasYsg.TabIndex = 1
         Me.tbpSintomasYsg.Text = "Signos clínicos y síntomas"
         '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.pnlSigno, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1312, 751)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnBorrarSintoma)
+        Me.Panel1.Controls.Add(Me.btnAgregarSintoma)
+        Me.Panel1.Controls.Add(Me.lblIngreseSintoma)
+        Me.Panel1.Controls.Add(Me.txtSintoma)
+        Me.Panel1.Controls.Add(Me.lbSintomas)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(659, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(650, 745)
+        Me.Panel1.TabIndex = 1
+        '
         'btnBorrarSintoma
         '
+        Me.btnBorrarSintoma.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnBorrarSintoma.BackgroundImage = CType(resources.GetObject("btnBorrarSintoma.BackgroundImage"), System.Drawing.Image)
         Me.btnBorrarSintoma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.btnBorrarSintoma.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnBorrarSintoma.Location = New System.Drawing.Point(1272, 22)
+        Me.btnBorrarSintoma.Location = New System.Drawing.Point(584, 8)
         Me.btnBorrarSintoma.Name = "btnBorrarSintoma"
-        Me.btnBorrarSintoma.Size = New System.Drawing.Size(40, 31)
-        Me.btnBorrarSintoma.TabIndex = 27
+        Me.btnBorrarSintoma.Size = New System.Drawing.Size(40, 30)
+        Me.btnBorrarSintoma.TabIndex = 41
         Me.btnBorrarSintoma.UseVisualStyleBackColor = True
-        '
-        'btnBorrarSigno
-        '
-        Me.btnBorrarSigno.BackgroundImage = CType(resources.GetObject("btnBorrarSigno.BackgroundImage"), System.Drawing.Image)
-        Me.btnBorrarSigno.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.btnBorrarSigno.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnBorrarSigno.Location = New System.Drawing.Point(565, 22)
-        Me.btnBorrarSigno.Name = "btnBorrarSigno"
-        Me.btnBorrarSigno.Size = New System.Drawing.Size(40, 30)
-        Me.btnBorrarSigno.TabIndex = 26
-        Me.btnBorrarSigno.UseVisualStyleBackColor = True
         '
         'btnAgregarSintoma
         '
+        Me.btnAgregarSintoma.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnAgregarSintoma.BackgroundImage = CType(resources.GetObject("btnAgregarSintoma.BackgroundImage"), System.Drawing.Image)
         Me.btnAgregarSintoma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.btnAgregarSintoma.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnAgregarSintoma.Location = New System.Drawing.Point(1226, 22)
+        Me.btnAgregarSintoma.Location = New System.Drawing.Point(538, 8)
         Me.btnAgregarSintoma.Name = "btnAgregarSintoma"
         Me.btnAgregarSintoma.Size = New System.Drawing.Size(40, 30)
-        Me.btnAgregarSintoma.TabIndex = 25
+        Me.btnAgregarSintoma.TabIndex = 40
         Me.btnAgregarSintoma.UseVisualStyleBackColor = True
         '
         'lblIngreseSintoma
         '
         Me.lblIngreseSintoma.AutoSize = True
         Me.lblIngreseSintoma.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblIngreseSintoma.Location = New System.Drawing.Point(714, 23)
+        Me.lblIngreseSintoma.Location = New System.Drawing.Point(26, 9)
         Me.lblIngreseSintoma.Name = "lblIngreseSintoma"
         Me.lblIngreseSintoma.Size = New System.Drawing.Size(190, 29)
-        Me.lblIngreseSintoma.TabIndex = 24
+        Me.lblIngreseSintoma.TabIndex = 39
         Me.lblIngreseSintoma.Text = "Ingrese sintoma:"
         '
         'txtSintoma
         '
+        Me.txtSintoma.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtSintoma.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtSintoma.Location = New System.Drawing.Point(910, 22)
+        Me.txtSintoma.Location = New System.Drawing.Point(242, 8)
         Me.txtSintoma.Name = "txtSintoma"
-        Me.txtSintoma.Size = New System.Drawing.Size(310, 30)
-        Me.txtSintoma.TabIndex = 23
+        Me.txtSintoma.Size = New System.Drawing.Size(290, 30)
+        Me.txtSintoma.TabIndex = 38
         '
         'lbSintomas
         '
+        Me.lbSintomas.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbSintomas.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbSintomas.FormattingEnabled = True
-        Me.lbSintomas.ItemHeight = 16
-        Me.lbSintomas.Location = New System.Drawing.Point(719, 74)
+        Me.lbSintomas.ItemHeight = 25
+        Me.lbSintomas.Location = New System.Drawing.Point(31, 60)
         Me.lbSintomas.Name = "lbSintomas"
-        Me.lbSintomas.Size = New System.Drawing.Size(593, 660)
-        Me.lbSintomas.TabIndex = 22
+        Me.lbSintomas.Size = New System.Drawing.Size(593, 654)
+        Me.lbSintomas.TabIndex = 37
+        '
+        'pnlSigno
+        '
+        Me.pnlSigno.Controls.Add(Me.btnBorrarSigno)
+        Me.pnlSigno.Controls.Add(Me.btnAgregarSClinico)
+        Me.pnlSigno.Controls.Add(Me.lblIngreseSignoClinico)
+        Me.pnlSigno.Controls.Add(Me.txtSgClinico)
+        Me.pnlSigno.Controls.Add(Me.lbSignosClinicos)
+        Me.pnlSigno.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlSigno.Location = New System.Drawing.Point(3, 3)
+        Me.pnlSigno.Name = "pnlSigno"
+        Me.pnlSigno.Size = New System.Drawing.Size(650, 745)
+        Me.pnlSigno.TabIndex = 0
+        '
+        'btnBorrarSigno
+        '
+        Me.btnBorrarSigno.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBorrarSigno.BackgroundImage = CType(resources.GetObject("btnBorrarSigno.BackgroundImage"), System.Drawing.Image)
+        Me.btnBorrarSigno.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnBorrarSigno.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBorrarSigno.Location = New System.Drawing.Point(584, 8)
+        Me.btnBorrarSigno.Name = "btnBorrarSigno"
+        Me.btnBorrarSigno.Size = New System.Drawing.Size(40, 30)
+        Me.btnBorrarSigno.TabIndex = 41
+        Me.btnBorrarSigno.UseVisualStyleBackColor = True
         '
         'btnAgregarSClinico
         '
+        Me.btnAgregarSClinico.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnAgregarSClinico.BackgroundImage = CType(resources.GetObject("btnAgregarSClinico.BackgroundImage"), System.Drawing.Image)
         Me.btnAgregarSClinico.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.btnAgregarSClinico.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnAgregarSClinico.Location = New System.Drawing.Point(519, 22)
+        Me.btnAgregarSClinico.Location = New System.Drawing.Point(538, 8)
         Me.btnAgregarSClinico.Name = "btnAgregarSClinico"
         Me.btnAgregarSClinico.Size = New System.Drawing.Size(40, 30)
-        Me.btnAgregarSClinico.TabIndex = 21
+        Me.btnAgregarSClinico.TabIndex = 40
         Me.btnAgregarSClinico.UseVisualStyleBackColor = True
         '
         'lblIngreseSignoClinico
         '
         Me.lblIngreseSignoClinico.AutoSize = True
         Me.lblIngreseSignoClinico.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblIngreseSignoClinico.Location = New System.Drawing.Point(7, 23)
+        Me.lblIngreseSignoClinico.Location = New System.Drawing.Point(26, 9)
         Me.lblIngreseSignoClinico.Name = "lblIngreseSignoClinico"
         Me.lblIngreseSignoClinico.Size = New System.Drawing.Size(240, 29)
-        Me.lblIngreseSignoClinico.TabIndex = 20
+        Me.lblIngreseSignoClinico.TabIndex = 39
         Me.lblIngreseSignoClinico.Text = "Ingrese signo clínico:"
         '
         'txtSgClinico
         '
+        Me.txtSgClinico.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtSgClinico.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtSgClinico.Location = New System.Drawing.Point(253, 22)
+        Me.txtSgClinico.Location = New System.Drawing.Point(289, 8)
         Me.txtSgClinico.Name = "txtSgClinico"
-        Me.txtSgClinico.Size = New System.Drawing.Size(260, 30)
-        Me.txtSgClinico.TabIndex = 19
+        Me.txtSgClinico.Size = New System.Drawing.Size(243, 30)
+        Me.txtSgClinico.TabIndex = 38
         '
         'lbSignosClinicos
         '
+        Me.lbSignosClinicos.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbSignosClinicos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbSignosClinicos.FormattingEnabled = True
-        Me.lbSignosClinicos.ItemHeight = 16
-        Me.lbSignosClinicos.Location = New System.Drawing.Point(12, 74)
+        Me.lbSignosClinicos.ItemHeight = 25
+        Me.lbSignosClinicos.Location = New System.Drawing.Point(31, 60)
         Me.lbSignosClinicos.Name = "lbSignosClinicos"
-        Me.lbSignosClinicos.Size = New System.Drawing.Size(593, 660)
-        Me.lbSignosClinicos.TabIndex = 18
+        Me.lbSignosClinicos.Size = New System.Drawing.Size(593, 654)
+        Me.lbSignosClinicos.TabIndex = 37
         '
         'SintomasYSClinicos
         '
@@ -190,7 +244,11 @@ Partial Class SintomasYSClinicos
         Me.Size = New System.Drawing.Size(1326, 786)
         Me.TabControlFormulario.ResumeLayout(False)
         Me.tbpSintomasYsg.ResumeLayout(False)
-        Me.tbpSintomasYsg.PerformLayout()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.pnlSigno.ResumeLayout(False)
+        Me.pnlSigno.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -198,14 +256,17 @@ Partial Class SintomasYSClinicos
     Friend WithEvents TabControlFormulario As TabControl
     Friend WithEvents tbpEntrevista As TabPage
     Friend WithEvents tbpSintomasYsg As TabPage
-    Friend WithEvents btnBorrarSintoma As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents pnlSigno As Panel
     Friend WithEvents btnBorrarSigno As Button
-    Friend WithEvents btnAgregarSintoma As Button
-    Friend WithEvents lblIngreseSintoma As Label
-    Friend WithEvents txtSintoma As TextBox
-    Friend WithEvents lbSintomas As ListBox
     Friend WithEvents btnAgregarSClinico As Button
     Friend WithEvents lblIngreseSignoClinico As Label
     Friend WithEvents txtSgClinico As TextBox
     Friend WithEvents lbSignosClinicos As ListBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnBorrarSintoma As Button
+    Friend WithEvents btnAgregarSintoma As Button
+    Friend WithEvents lblIngreseSintoma As Label
+    Friend WithEvents txtSintoma As TextBox
+    Friend WithEvents lbSintomas As ListBox
 End Class
